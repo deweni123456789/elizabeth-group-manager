2f60d10f75f760043dda1661920e5a7088ad65db
deweni123456789 <schoford91@gmail.com>
2025-08-28 22:14:39 +0530

    Update backup.py

--- modules/backup.py
+++ modules/backup.py
@@ -1,3 +1,20 @@
+from pyrogram import filters
+from pyrogram.types import Message
+from .utils import developer_button
+import config
+
 def register(app):
-    # Placeholder for backing up messages or settings
-    pass
+    @app.on_message(filters.private)
+    async def force_sub_check(client, message: Message):
+        user = message.from_user
+        try:
+            member = await client.get_chat_member(config.FORCE_CHANNEL, user.id)
+            if member.status in ["member", "administrator", "creator"]:
+                return
+        except:
+            pass
+
+        await message.reply_text(
+            "You must join our channel to use the bot!",
+            reply_markup=developer_button()
+        )
