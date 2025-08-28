76419834e360d9df5c016319677a5fd439d8aff8
deweni123456789 <schoford91@gmail.com>
2025-08-28 21:53:28 +0530

    Update config.py

--- config.py
+++ config.py
@@ -1,10 +1,10 @@
 import os
 
-API_ID = int(os.getenv("API_ID", 0))
-API_HASH = os.getenv("API_HASH", "")
-BOT_TOKEN = os.getenv("BOT_TOKEN", "")
+API_ID = int(os.getenv("API_ID", 5047271))
+API_HASH = os.getenv("API_HASH", "047d9ed308172e637d4265e1d9ef0c27")
+BOT_TOKEN = os.getenv("BOT_TOKEN", "8400227620:AAEje87350ILP5lJxMeb57rOn9K8yBY1f2o")
 ADMIN_USERS = {int(x) for x in os.getenv("ADMIN_USERS","").split(",") if x}
-FORCE_CHANNEL = os.getenv("FORCE_CHANNEL", "@YourChannel")
+FORCE_CHANNEL = os.getenv("FORCE_CHANNEL", "@Slmusicmania")
 LOG_CHAT_ID = int(os.getenv("LOG_CHAT_ID", 0))
 
 ENABLE_WELCOME = os.getenv("ENABLE_WELCOME", "1") == "1"
